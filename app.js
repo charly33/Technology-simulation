39b77b68af1f3573ef2ed5adc79727d60a2473b9
monnier.charly@orange.fr <32869761+charly33@users.noreply.github.com>
2018-12-20 02:27:57 +0100

    Done

--- app.js
+++ app.js
@@ -1,41 +1,115 @@
-
+/////ANIMATION MAN////////
     // define images
-	 images = [
-		"img/4.png",
-		"img/3.png",
-        "img/2.png",
-        "img/1.png"
+	 images1 = [
+		"img/marche1-min.png",
+		"img/marche2-min.png",
+        "img/marche3-min.png",
+        "img/marche4-min.png"
 	];
 
-	var obj = {curImg: 0};
+	var obj1 = {curImg: 0};
 
 	// create tween
-	var tween = TweenMax.to(obj, 0.5,
+	var tween1 = TweenMax.to(obj1, 0.5,
 		{
-			curImg: images.length - 1,	// animate propery curImg to number of images
+			curImg: images1.length - 1,	// animate propery curImg to number of images
 			roundProps: "curImg",				// only integers so it can be used as an array index
-			repeat: 90,									// repeat 3 times
+			repeat: 20,									// repeat 3 times
 			immediateRender: true,			// load first image automatically
 			ease: Linear.easeNone,			// show every image the same ammount of time
 			onUpdate: function () {
-			  $("#myimg").attr("src", images[obj.curImg]); // set the image source
+			  $("#myimg").attr("src", images1[obj1.curImg]); // set the image source
 			}
 		}
 	);
 
 	// init controller
-    var controller = new ScrollMagic.Controller({vertical: false});
+    var controller1 = new ScrollMagic.Controller({vertical: false});
     
 	// build scene
-    var scene = new ScrollMagic.Scene({triggerElement: ".background", duration: 48200})
-                    .setTween(tween)
-					.addTo(controller);
+    var scene1 = new ScrollMagic.Scene({triggerElement: ".scene1", duration: 17000})
+                    .setTween(tween1)
+					.addTo(controller1);
 
 	// handle form change
 	$("form.move input[name=duration]:radio").change(function () {
-		scene.duration($(this).val());
+		scene1.duration($(this).val());
     });
 
+    ////////
+    // define images
+     images2 = [
+		"img/music1-min.png",
+		"img/music2-min.png",
+        "img/music3-min.png",
+        "img/music4-min.png"
+	];
+
+	var obj2 = {curImg: 0};
+
+	// create tween
+	var tween2 = TweenMax.to(obj2, 0.5,
+		{
+			curImg: images2.length - 1,	// animate propery curImg to number of images
+			roundProps: "curImg",				// only integers so it can be used as an array index
+			repeat: 10,									// repeat 3 times
+			immediateRender: true,			// load first image automatically
+			ease: Linear.easeNone,			// show every image the same ammount of time
+			onUpdate: function () {
+			  $("#myimg").attr("src", images2[obj2.curImg]); // set the image source
+			}
+		}
+	);
+
+	// init controller
+    var controller2 = new ScrollMagic.Controller({vertical: false});
+    
+	// build scene
+    var scene2 = new ScrollMagic.Scene({triggerElement: ".scene8", duration: 8500})
+                    .setTween(tween2)
+					.addTo(controller2);
+
+	// handle form change
+	$("form.move input[name=duration]:radio").change(function () {
+		scene2.duration($(this).val());
+    });
+//////
+  // define images
+  images3 = [
+    "img/tel4-min.png",
+    "img/tel4-min.png",
+    "img/tel2-min.png",
+    "img/tel1-min.png"
+];
+
+var obj3 = {curImg: 0};
+
+// create tween
+var tween3 = TweenMax.to(obj3, 0.5,
+    {
+        curImg: images3.length - 1,	// animate propery curImg to number of images
+        roundProps: "curImg",				// only integers so it can be used as an array index
+        repeat: 20,									// repeat 3 times
+        immediateRender: true,			// load first image automatically
+        ease: Linear.easeNone,			// show every image the same ammount of time
+        onUpdate: function () {
+          $("#myimg").attr("src", images3[obj3.curImg]); // set the image source
+        }
+    }
+);
+
+// init controller
+var controller3 = new ScrollMagic.Controller({vertical: false});
+
+// build scene
+var scene3 = new ScrollMagic.Scene({triggerElement: ".scene12", duration: 9500})
+                .setTween(tween3)
+                .addTo(controller3);
+
+// handle form change
+$("form.move input[name=duration]:radio").change(function () {
+    scene3.duration($(this).val());
+});
  /////////////////////////////////
 
  // get all slides
@@ -44,15 +118,20 @@ const slides = document.querySelectorAll(".scenes")
 
 const scrollscenes = () =>
  { 
-		// init
+        // init
+
 		var controller = new ScrollMagic.Controller(
         {
+
             globalSceneOptions: 
             {
+                reverse:true,
                 triggerHook: 'onLeave',
             },
+
             vertical:false,
         })
+        
 
 		// create scene for every slide
         for (var i=0; i<slides.length; i++) 
@@ -60,20 +139,45 @@ const scrollscenes = () =>
 			new ScrollMagic.Scene(
                 {
                     triggerElement: slides[i],
-                    duration:1000
+                    duration:900
                     
                 })
                 .setPin(slides[i])
 				 // add indicators (requires plugin)
                 .addTo(controller)
-        }  
+                
+        } 
         
- }
-    
+ }   
 scrollscenes()
+
 
 const illustrationAnimation = () =>
 {
+    /* SCENES1 */
+///IMG ANIMATION/////
+var controller = new ScrollMagic.Controller({vertical: false})
+
+var ourScene = new ScrollMagic.Scene({
+    triggerElement : '.introtext',
+    triggerHook:0,
+    duration:'99%',
+})
+.setClassToggle('.banner','fade-out')
+.addTo(controller)
+
+///IMG ANIMATION/////
+var controller = new ScrollMagic.Controller({vertical: false})
+
+var ourScene = new ScrollMagic.Scene({
+    triggerElement : '.introtext',
+    triggerHook:0,
+    duration:'99%',
+})
+.setClassToggle('.introtext','fade-out')
+.addTo(controller)
+ 
+//////////////////////////
 /* SCENES2 */
 ///IMG ANIMATION/////
     var controller = new ScrollMagic.Controller({vertical: false})
@@ -115,6 +219,28 @@ var textScene = new ScrollMagic.Scene({
     duration:'99%',
 })
 .setClassToggle('.text3','zoom-in')
+.addTo(textcontroller) 
+//////////////////////////
+
+/* SCENES3B */
+///IMG ANIMATION/////
+var controller = new ScrollMagic.Controller({vertical: false})
+
+var ourScene = new ScrollMagic.Scene({
+    triggerElement : '.scene3-B',
+    triggerHook:0,
+    duration:'99%',
+})
+.setClassToggle('.illustration3-B','fade-in')
+.addTo(controller)
+/////TEXT ANIMATION////
+var textcontroller = new ScrollMagic.Controller({vertical: false})
+var textScene = new ScrollMagic.Scene({
+    triggerElement : '.scene3-B',
+    triggerHook:0,
+    duration:'99%',
+})
+.setClassToggle('.text3-B','zoom-in')
 .addTo(textcontroller) 
 //////////////////////////
 
@@ -316,27 +442,6 @@ var textScene = new ScrollMagic.Scene({
 .addTo(textcontroller) 
 //////////////////////////
 
-/* SCENES13 */
-///IMG ANIMATION/////
-var controller = new ScrollMagic.Controller({vertical: false})
-
-var ourScene = new ScrollMagic.Scene({
-    triggerElement : '.scene13',
-    triggerHook:0,
-    duration:'99%',
-})
-.setClassToggle('.illustration13','fade-in')
-.addTo(controller)
-/////TEXT ANIMATION////
-var textcontroller = new ScrollMagic.Controller({vertical: false})
-var textScene = new ScrollMagic.Scene({
-    triggerElement : '.scene13',
-    triggerHook:0,
-    duration:'99%',
-})
-.setClassToggle('.text13','zoom-in')
-.addTo(textcontroller) 
-//////////////////////////
 
 /* SCENES14 */
 ///IMG ANIMATION/////
@@ -415,7 +520,7 @@ window.addEventListener('scroll',_event => {
     scrollPos= window.pageXOffset
     console.log(scrollPos)
     for(i=0; i<16 ; i++){
-        if(scrollPos>2225*i)
+        if(scrollPos>2270*i)
         {
             li[i].classList.add('complete')
         }
